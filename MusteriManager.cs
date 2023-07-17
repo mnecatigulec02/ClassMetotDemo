12f3491fa3988f90e79ff221a1e44b9e580b6db3
ngule <ngule@DESKTOP-PVEILPL>
2023-07-17 16:45:54 +0300

    Add project files.

---
+++ MusteriManager.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMetotDemo
+{
+    internal class MusteriManager
+    {        
+        List<Musteri> musteriList =new List<Musteri>();
+        public void MusteriEkle(Musteri musteriExp)
+        {
+            musteriList.Add(musteriExp);
+            Console.WriteLine(musteriExp.Ad+" eklendi.");
+        }        
+        public void MusterileriListele()
+        {
+            foreach (Musteri x in musteriList)
+            {
+                Console.WriteLine("Müşteri id = "+x.Id+", adi = "+x.Ad+", yaşı = "+x.Yas+".");
+            }
+        }
+        public void MusteriSil(Musteri musteriExp)
+        {
+            musteriList.Remove(musteriExp);
+            Console.WriteLine(musteriExp.Ad + " silindi.");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿namespace ClassMetotDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri musteri1 = new Musteri();
+            musteri1.Id = 1;
+            musteri1.Ad = "Mehmet";
+            musteri1.Yas = 21;
+
+            Musteri musteri2 = new Musteri();
+            musteri2.Id = 2;
+            musteri2.Ad = "Yusuf";
+            musteri2.Yas = 19;
+
+            MusteriManager m = new MusteriManager();
+            m.MusteriEkle(musteri1);
+            m.MusteriEkle(musteri2);
+
+            Console.WriteLine("------------------");
+
+            m.MusterileriListele();
+            
+            Console.WriteLine("------------------");
+
+            m.MusteriSil(musteri1);
+            m.MusteriSil(musteri2);
+        }
+    }
+}
